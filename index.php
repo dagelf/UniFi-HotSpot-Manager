07db30576284ba7b59dd475aafd99743b31f002b
Coenraad Loubser <coenraad@wish.org.za>
2018-03-10 11:48:30 +0200

    Update index.php
    Best to have this here for portability eg. running in a docker instance.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * nearly no files in real web space
  */
 define('HOTSPOT', true);
-
+date_default_timezone_set('Africa/Johannesburg'); // Update this
 $PATH_PRIVATE = "/srv/http/hotspot/private";
 require("$PATH_PRIVATE/hotspot.php");
 
